e0009d49167af77a210341383aed9a179d019661
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-12-06 07:12:53 +0300

    add seeds data

---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+User.create([
+{ username: "Duncan",
+    email: "dancherry98@gmail.com",
+    password: "111111"
+
+},
+
+{ username: "Peter",
+    email: "gaggagg98@gmail.com",
+    password: "662363636"
+
+},
+
+{ username: "Enock",
+  email: "dhahagg@gmail.com",
+  password: "6463636"
+
+},
+{ username: "Daah",
+email: "vvvavav@gmail.com",
+password: "5335355"
+
+}
+])
