30f82c0a1fe856ead2078e0fc5d6a664d7f7b1b9
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-12-07 12:58:33 +0300

    made changes and delete skip auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-    # skip_before_action :verify_authenticity_token 
-    # before_action :authorize, only: [:show]
-  
     def create
       user = User.create!(user_params)
       session[:user_id] = user.id
@@ -19,7 +16,8 @@ class UsersController < ApplicationController
     private
   
     def user_params
-      params.permit(:username, :email, :password, :password_confirmation)
+      params.permit(:username, :password, :password_confirmation)
     end
   
   end
+
