e7ff581d6daef2cd3cf84d4d03ce28b4d1ca03bf
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-12-08 08:21:01 +0300

    add method destroy and update

--- app/controllers/cows_controller.rb
+++ app/controllers/cows_controller.rb
@@ -1,29 +1,44 @@
 class CowsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-    def index
-        cows = Cow.all
-        render json: cows
-      end
-    
-      def show
-        cow = Cow.find(params[:id])
-        render json: cow
-      end
-    
-      def create
-        cow = Cow.create(cow_params)
-        render json: cow, status: :created
-      end
-  
+      def index
+          cows = Cow.all
+          render json: cows
+        end
+      
+        def show
+          cow = Cow.find(params[:id])
+          render json: cow
+        end
+      
+        def create
+          cow = Cow.create(cow_params)
+          render json: cow, status: :created
+        end
+          def update
+            cow = Cow.find(params[:id])
+            cow.update(update_price)
+            render json: cow
+        end
 
-      private
-
-      def render_not_found_response
-        render json: {error: "Cow not Found"}, status: :not_found
-      end
+        def destroy
+          cow=Cow.find(params[:id])
+          cow.destroy
+          head :no_content
+        end
     
-      def cow_params
-        params.permit(:name, :image, :price)
-      end
+
+        private
+
+        def render_not_found_response
+          render json: {error: "Cow not Found"}, status: :not_found
+        end
+      
+        def cow_params
+          params.permit(:name, :image, :price)
+        end
+
+        def update_price
+          params.permit(:price)
+        end
 
 end
